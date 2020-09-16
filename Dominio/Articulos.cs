56820d0f9476798805101dda58bff6f782605da4
CarlosJesus <c.sipiran07@gmail.com>
2020-09-16 20:14:54 -0300

    Armado de Dominio de clases y desarrollo de la funcion Listar

---
+++ Dominio/Articulos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dominio
+{
+    public class Articulos
+    {
+        private int codArticulo;
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Catgoria { get; set; }
+        public string Imagen { get; set; }
+        public float Precio { get; set; }
+        Categoria
+        Marca;
+    }
+    
+}
---
+++ Negocio/ArticuloNegocio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulos> listar()
+        {
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            List<Articulos> lista = new List<Articulos>();
+            
+            conexion.ConnectionString = "data source = CAJS0718; initial catalog = CATALOGO_DB; integrated security = sspi";
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = "SELECT Nombre FROM ARTICULOS";
+            comando.Connection = conexion;
+
+            conexion.Open();
+            lector = comando.ExecuteReader();
+
+            while(lector.Read())
+            {
+                Articulos aux = new Articulos();
+                aux.Nombre = lector.GetString(0);
+                lista.Add(aux);
+            }
+            conexion.Close();
+            return lista;
+        }
+    }
+}
